76f850ade5f73bd48dfe7712abc0458d93dda9c2
Teutonos <iammessiah@ya.ru>
2015-07-21 10:12:39 +0300

    added Tetris class

--- tetris.py
+++ tetris.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python
-# -*- coding: latin-1 -*-
 
-import os
-from msvcrt import getch
-
-from step import step
-from draw import *
-from core import *
-
-ESC = 27
+import core
+import random
 
 class Tetris:
-    __init__
+    def __init__(self):
+        self.field = [[0 for col in range(10)] for row in range(21)]
+        self.nextPiece = random.choice(core.pieces.keys)
+        self.newRound = True
 
+    def step(self):
+        if self.newRound: 
+            for col in len(self.field[0]):
+                if self.field[0][col] != 0:
+                    self.gameOver()
+                    return
+            self.piece = self.nextPiece
+            self.x = 5 - (len(core.pieces[self.piece]) + 1)/2
+            self.y = 0
+            self.nextPiece = random.choice(core.pieces.keys)
+            self.newRound = False
+        write()
 
-if __name__ == "__main__":
-    draw(rotate(pieces['I']))
-    tetris()
-
+    def gameOver(self)
+        pass
